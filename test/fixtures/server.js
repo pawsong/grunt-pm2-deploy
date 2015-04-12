52bf2e5690a53879708dec83cccf66cb8c803846
Giff Song <giff.song@gmail.com>
2015-04-12 21:01:56 +0900

    Implement test server

--- test/fixtures/server.js
+++ test/fixtures/server.js
@@ -1,2 +1,13 @@
 'use strict';
 
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(JSON.stringify({
+    env: process.env.NODE_ENV
+  }));
+}).listen(port, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:%d/', port);
