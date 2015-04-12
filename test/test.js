c6c828a7420563ce302c7bdb679c1277cc43c7c8
Giff Song <giff.song@gmail.com>
2015-04-12 22:22:32 +0900

    refactoring

--- test/test.js
+++ test/test.js
@@ -20,58 +20,7 @@ var ROOT_PATH = path.resolve(__dirname, '..');
 
 describe('pm2deploy', function () {
 
-  describe('#grunt-task', function () {
-
-    before(function (done) {
-
-      require('../Gruntfile')(grunt);
-
-      // Run ssh server
-      sshSrv(ROOT_PATH, function (err, port) {
-        if (err) {
-          return done(err);
-        }
-
-        // Set ssh port as grunt config
-        grunt.config.set('pm2deploy.options.defaultConfig.port', '' + port);
-        done();
-      });
-    });
-
-    it('should deploy server', function (done) {
-
-      runTask.task('pm2deploy:test', grunt.config.get('pm2deploy')).run(done);
-    });
-  });
-
-  describe('#deployed-server', function () {
-
-    it('should return correct response', function (done) {
-      
-      request('http://127.0.0.1:10123', function (err, response, body) {
-        if (err) {
-          return done(err); 
-        }
-
-        if (response.statusCode !== 200) {
-          return done(new Error('Invalid statusCode: ' + response.statusCode)); 
-        }
-
-        try {
-          body = JSON.parse(body);
-          expect(body).to.be.deep.equal({
-            env: 'test' 
-          });
-        } catch(e) {
-          return done(e); 
-        }
-
-        return done();
-      })
-    });
-  });
-
-  after(function (done) {
+  function cleanUp (done) {
 
     async.series([
 
@@ -80,7 +29,9 @@ describe('pm2deploy', function () {
         pm2.connect(callback);
       },
       function (callback) {
-        pm2.delete('grunt-pm2-deploy', callback);
+        pm2.delete('grunt-pm2-deploy', function () {
+          callback();
+        });
       },
       function (callback) {
         pm2.disconnect(callback);
@@ -91,6 +42,69 @@ describe('pm2deploy', function () {
         rimraf('/tmp/grunt-pm2-deploy', callback);
       }
     ], done);
+  }
+
+  before(function (done) {
+
+    async.waterfall([
+
+      cleanUp,
+
+      // Start ssh server
+      function (callback) {
+        sshSrv(ROOT_PATH, callback);
+      },
+
+      // Set ssh port as grunt config
+      function (port, callback) {
+
+        require('../Gruntfile')(grunt);
+
+        grunt.config.set('pm2deploy.options.defaultConfig.port', '' + port);
+        callback();
+      }
+    ], done);
   });
+
+  describe('#grunt-task', function () {
+
+    it('should deploy server', function (done) {
+
+      runTask.task('pm2deploy:test', grunt.config.get('pm2deploy')).run(done);
+    });
+  });
+
+  describe('#deployed-server', function () {
+
+    it('should return correct response', function (done) {
+
+      async.waterfall([
+
+        function (callback) {
+          request('http://127.0.0.1:10123', callback);
+        },
+
+        function (response, body, callback) {
+
+          if (response.statusCode !== 200) {
+            return callback(new Error('Invalid statusCode: ' + response.statusCode));
+          }
+
+          try {
+            body = JSON.parse(body);
+            expect(body).to.be.deep.equal({
+              env: 'test'
+            });
+          } catch(e) {
+            return callback(e);
+          }
+
+          return callback();
+        }
+      ], done);
+    });
+  });
+
+  after(cleanUp);
 });
 
