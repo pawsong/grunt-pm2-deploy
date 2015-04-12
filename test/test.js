eb2b7f1bbc898cdd139291169af40b1b7604d1e9
Giff Song <giff.song@gmail.com>
2015-04-12 21:46:12 +0900

    Add deployed server test

--- test/test.js
+++ test/test.js
@@ -5,10 +5,10 @@
 var expect = require('chai').expect;
 
 var grunt = require('grunt'),
-    async = require('async'),
     path = require('path'),
     fs = require('fs'),
     runTask = require('grunt-run-task'),
+    request = require('request'),
     format = require('util').format;
 
 var sshSrv = require('./lib/sshServer');
@@ -19,26 +19,55 @@ var ROOT_PATH = path.resolve(__dirname, '..');
 
 describe('pm2deploy', function () {
 
-  before(function (done) {
+  describe('#grunt-task', function () {
 
-    require('../Gruntfile')(grunt);
+    before(function (done) {
 
-    // Run ssh server
-    sshSrv(ROOT_PATH, function (err, port) {
-      if (err) {
-        return done(err);
-      }
+      require('../Gruntfile')(grunt);
 
-      // Set ssh port as grunt config
-      grunt.config.set('pm2deploy.options.defaultConfig.port', '' + port);
-      done();
+      // Run ssh server
+      sshSrv(ROOT_PATH, function (err, port) {
+        if (err) {
+          return done(err);
+        }
+
+        // Set ssh port as grunt config
+        grunt.config.set('pm2deploy.options.defaultConfig.port', '' + port);
+        done();
+      });
+    });
+
+    it('should deploy server', function (done) {
+
+      runTask.task('pm2deploy:test', grunt.config.get('pm2deploy')).run(done);
     });
   });
 
-  it('should generate config file for given env', function (done) {
+  describe('#deployed-server', function () {
 
-    runTask.task('pm2deploy:test', grunt.config.get('pm2deploy')).run(done);
+    it('should return correct response', function (done) {
+      
+      request('http://127.0.0.1:10123', function (err, response, body) {
+        if (err) {
+          return done(err); 
+        }
+
+        if (response.statusCode !== 200) {
+          return done(new Error('Invalid statusCode: ' + response.statusCode)); 
+        }
+
+        try {
+          body = JSON.parse(body);
+          expect(body).to.be.deep.equal({
+            env: 'test' 
+          });
+        } catch(e) {
+          return done(e); 
+        }
+
+        return done();
+      })
+    });
   });
-
 });
 
